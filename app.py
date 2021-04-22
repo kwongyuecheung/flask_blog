0a3bf4973b818e13637d7d05c19329714818d5fe
kwongyuecheung <leokwong92@gmail.com>
2021-04-22 20:54:10 +0900

    no message

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-app = Flask(__name__)
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required,current_user
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
 db = SQLAlchemy(app)
+
+
+
+
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(30))
+
+# #login functions
+# @login_manager.user_loader
+# def load_user(username):
+#     if username not in User:
+#         return
+#     user = User()
+#     user.username = username
+#     return user
+
+
 
 class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -46,5 +67,18 @@ def addpost():
     db.session.commit()
     
     return redirect(url_for('index'))
+
+# @app.route('/login',methods =['POST'])
+# def login():
+#     # if request.method =='GET':
+#     #     return render_template('login.html')
+#     inputuser = request.form['inputuserid']
+#     if (inputuser in User) and (request.form['password']==User[inputuser]['password']):
+#         user = User()
+#         user.id = inputuser
+#         login_user(user)
+#         return redirect(url_for('add.html'))
+#     return render_template('login.html')
+
 if __name__=='__main__':
     app.run(debug=True)
